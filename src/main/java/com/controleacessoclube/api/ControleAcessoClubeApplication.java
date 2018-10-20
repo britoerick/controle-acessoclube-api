320929cf9ef03fe103de29a2fa25407534d61343
Erick Brito <erickdebrito@hotmail.com>
2018-10-20 11:24:24 -0300

    commit do projeto inicial

---
+++ src/main/java/com/controleacessoclube/api/ControleAcessoClubeApplication.java
@@ -0,0 +1,12 @@
+package com.controleacessoclube.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleAcessoClubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleAcessoClubeApplication.class, args);
+	}
+}
